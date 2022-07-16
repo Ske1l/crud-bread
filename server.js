b8d11dd413125be57cd8930850384e9aa6b440e3
Ske1l <hayley6236@gmail.com>
2022-07-16 12:31:50 -0400

    bread

---
+++ server.js
@@ -0,0 +1,23 @@
+//dependencies
+const express = require('express')
+
+//configuration
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Welcome to an awesome app about breads!')
+})
+
+//breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+//listen
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+})
+
