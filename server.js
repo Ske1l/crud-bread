3ddac548debb5ddfe57d4d104b36a43902261cc5
Ske1l <hayley6236@gmail.com>
2022-07-18 19:01:58 -0400

    sending index and show data, added links to normalize and skeleton

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -26,3 +27,7 @@ app.listen(PORT, () => {
     console.log('listening on port', PORT);
 })
 
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+})
