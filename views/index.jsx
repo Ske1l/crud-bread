d0575a19b38c5cee52437dc0d4e2fb461fde21b7
Ske1l <hayley6236@gmail.com>
2022-08-06 00:53:57 -0400

    added show page for bakers

--- views/index.jsx
+++ views/index.jsx
@@ -1,23 +1,24 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Index({ breads }) {
+function Index({ breads, bakers, title }) {
     return (
         <Default>
             <h2>Index Page</h2>
             {/* <p>I have {breads[0].name} bread!</p> */}
+            <h3>Bakers</h3>
             <ul>
                 {
-                    breads.map((bread, index) => {
+                    bakers.map((baker) => {
                         return (
-                            <li key={index}>
-                                <a href={`/breads/${bread.id}`}>
-                                    {bread.name}
-                                </a>
-                            </li>)
+                            <li key={baker.id}>
+                                <a href={`/bakers/${baker.id}`}>{baker.name}</a>
+                            </li>
+                        )
                     })
                 }
             </ul>
+            <h3>Breads</h3>
             <div className="newButton">
                 <a href="/breads/new"><button>Add a new bread</button></a>
             </div>
@@ -27,4 +28,5 @@ function Index({ breads }) {
 }
 
 module.exports = Index
+
 
