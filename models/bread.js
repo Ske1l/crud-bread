7237d03cf7486d58c9b88996cb38269f5a68bbce
Ske1l <hayley6236@gmail.com>
2022-08-05 22:43:12 -0400

    added bakers to list

--- models/bread.js
+++ models/bread.js
@@ -31,9 +31,14 @@ const { Schema } = mongoose
 
 const breadSchema = new Schema({
     name: { type: String, required: true },
-    hasGluten: { type: Boolean },
-    image: { type: String, default: 'http://placehold.it/500x500.png' }
+    hasGluten: Boolean,
+    image: { type: String, default: 'http://placehold.it/500x500.png' },
+    baker: {
+        type: String,
+        enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
+    }
 })
+
 
 const Bread = mongoose.model('Bread', breadSchema)
 
